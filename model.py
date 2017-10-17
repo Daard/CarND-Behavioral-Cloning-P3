a5d8e1b0ac1fabc1afd4edc6e20f3677568fc100
Daard <larion.babych@gmail.com>
2017-10-17 19:02:24 +0300

    copy preprocess from host

--- model.py
+++ model.py
@@ -16,6 +16,18 @@ with open(data_folder + '/driving_log.csv') as csvfile:
         samples.append(line)
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+def preprocess(image):
+    # I used nvidia's preprocess function
+    # Crop
+    cropped  = image[70:135,10:350]
+    # Resize
+    resized = cv2.resize(cropped, (200, 66), interpolation=cv2.INTER_AREA)
+    # Blur
+    blurred = cv2.GaussianBlur(resized, (5,5), 0)
+    # Convert to YUV
+    final_image = cv2.cvtColor(blurred, cv2.COLOR_BGR2YUV)
+    return final_image
+    
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -28,8 +40,9 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 for i in range(3):
                     name = data_folder + 'IMG/' + batch_sample[i].split('/')[-1]
-                    center_image = cv2.imread(name)
-                    images.append(center_image)
+                    #nvidia's preprocess image function
+                    image = preprocess(cv2.imread(name))
+                    images.append(image)
                 center_angle = float(batch_sample[3])
                 angles.extend([center_angle, center_angle + 0.2, center_angle - 0.2])
             augmented_images, augmented_angles = [], []
@@ -45,8 +58,8 @@ def generator(samples, batch_size=32):
 
 def model():
     model = Sequential()
-    model.add(Cropping2D(((70, 25), (0, 0)), input_shape=(160, 320, 3)))
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+   # model.add(Cropping2D(((70, 25), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(66, 200, 3), output_shape=(66, 200, 3)))
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='elu'))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='elu'))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='elu'))
@@ -54,11 +67,11 @@ def model():
     model.add(Convolution2D(64, 3, 3, activation='elu'))
     model.add(Flatten())
     model.add(Dense(100))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     model.add(Dense(50))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     model.add(Dense(10))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     model.add(Dense(1))
     return model
 
@@ -67,8 +80,9 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 model = model()
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, samples_per_epoch=6*len(train_samples), validation_data=validation_generator,
-                    nb_val_samples=6*len(validation_samples), nb_epoch=10)
+history_object = model.fit_generator(train_generator, samples_per_epoch=6 * len(train_samples), validation_data=validation_generator,
+                    nb_val_samples=6 * len(validation_samples), nb_epoch=30)
+
 model.save('model.h5')
 
 print(history_object.history.keys())
