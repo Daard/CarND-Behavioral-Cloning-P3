0ab8374f5ccd0a52103bc1cfa852e9689dbcb71f
Daard <larion.babych@gmail.com>
2017-10-17 07:25:24 +0300

    elu, dropout

--- model.py
+++ model.py
@@ -2,14 +2,14 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 samples = []
-data_folder = '/Users/daard/Documents/CarND/data-p3/'
-# data_folder = '/home/carnd/data/'
+#data_folder = '/Users/daard/Documents/CarND/data-p3/'
+data_folder = '/home/carnd/data/'
 with open(data_folder + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
@@ -47,15 +47,18 @@ def model():
     model = Sequential()
     model.add(Cropping2D(((70, 25), (0, 0)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='elu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='elu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='elu'))
+    model.add(Convolution2D(64, 3, 3, activation='elu'))
+    model.add(Convolution2D(64, 3, 3, activation='elu'))
     model.add(Flatten())
     model.add(Dense(100))
+    model.add(Dropout(0.3))
     model.add(Dense(50))
+    model.add(Dropout(0.3))
     model.add(Dense(10))
+    model.add(Dropout(0.3))
     model.add(Dense(1))
     return model
 
@@ -64,10 +67,15 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 model = model()
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples), nb_epoch=3)
-
+history_object = model.fit_generator(train_generator, samples_per_epoch=6*len(train_samples), validation_data=validation_generator,
+                    nb_val_samples=6*len(validation_samples), nb_epoch=10)
 model.save('model.h5')
+
+print(history_object.history.keys())
+print('Loss')
+print(history_object.history['loss'])
+print('Validation Loss')
+print(history_object.history['val_loss'])
 
 # TODO: generate more data :
 # two or three laps of center lane driving
